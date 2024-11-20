9e5eae54a805a553c25338817df0e4df9645df58
NAJIM EL HOUSSINE <334694@studenti.unimore.it>
2024-11-20 16:23:44 +0100

    main

---
+++ ex3.c
@@ -0,0 +1,9 @@
+ #include <stdio.h>
+
+int main() {
+     int base = 4, altezza = 7;
+     float area;
+     area = (float)(base * altezza) / 2.0F;
+     printf("area = %f\n");
+
+}
---
+++ ex5.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main() {
+    int a = 4 ;
+    if ( a >= 0) {
+        printf("a e positivo\n");
+    } else {
+        printf("a e negativo\n");
+    }
+    if (a % 2==0) {
+        printf("a e pari\n");
+    } else {
+        printf("a e dispari\n ");
+    }
+}
