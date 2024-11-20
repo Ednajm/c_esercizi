e308e066fa8729705aea3b9ce08c1209a52c7795
NAJIM EL HOUSSINE <334694@studenti.unimore.it>
2024-11-20 16:28:22 +0100

    main

---
+++ ex9.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+#include<math.h>
+
+
+int main() {
+    int log_approx , n = 4;
+    log_approx= (int)floor(log10((double)n));
+    printf("log approssimato =%d\n", log_approx);
+}
