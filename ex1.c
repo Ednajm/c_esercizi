6fd04e721883a07c52b2f8bdba18aa31fa197e84
NAJIM EL HOUSSINE <334694@studenti.unimore.it>
2024-11-20 16:11:13 +0100

    ex1

--- ex1.c
+++ ex1.c
@@ -1,12 +1,11 @@
-#include<stdio.h>
+#include <stdio.h>
 
 int main() {
-    unsigned int n = 117, cols = 5;
-    
-    for(unsigned int i = 0; i <= n; i++) {
-        if (i % cols == 0) {
-            printf(" \n");
-        }
-        printf("%4u ", i);
-    }
+
+    int base = 10, altezza = 3 ,area;
+
+    area = base * altezza;
+    printf("area=%d\n");
+
+    return 0;
 }
