e0a182fb35a6297ecaa6facc24a20c67663477e4
NAJIM EL HOUSSINE <334694@studenti.unimore.it>
2024-12-23 16:29:18 +0100

    Added new feature

--- lab03/ex03.c
+++ lab03/ex03.c
@@ -15,14 +15,11 @@ void tabula_recta(char t[][26]){
 int main(void) {
     int i, j;
     char t[SIZE][SIZE];
-
     tabula_recta(t);
-
-    /* show tabula recta */
     for (i = 0; i < SIZE; i++) {
         for (j = 0; j < SIZE; j++) {
-            printf("%2c", t[i][j]);
+            printf("%c ", t[i][j]);
         }
         printf("\n");
-    }
-}
+    } 
+    
