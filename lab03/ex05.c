84f96dc11d4025d4bc2423eb417417a184626222
Your GitHub Ednajm <334694@studenti.unimore.it>
2024-12-24 21:47:15 +0100

    new

--- lab03/ex05.c
+++ lab03/ex05.c
@@ -8,34 +8,33 @@ struct array_info {
     float variance;
 };
 
-struct array_info array_stats(const int *values, unsigned size) {
+void array_stats(struct array_info *r, const int *values, unsigned size) {
     unsigned i;
-    int max = values[0], min = values[0];
-    float mean = 0.0F, variance = 0.0F;
+
+    r->max = values[0];
+    r->min = values[0];
+    r->mean = 0.0F;
+    r->variance = 0.0F;
 
     /* compute max, min, sum */
     for (i = 0; i < size; i++) {
-        if (values[i] > max) {
-            max = values[i];
+        if (values[i] > r->max) {
+            r->max = values[i];
         }
-        if (values[i] < min) {
-            min = values[i];
+        if (values[i] < r->min) {
+            r->min = values[i];
         }
-        mean += (float) values[i];
+        r->mean += (float) values[i];
     }
 
     /* compute mean */
-    mean /= (float) size;
+    r->mean /= (float) size;
 
     /* compute variance */
     for (i = 0; i < size; i++) {
-        variance += powf((float) values[i] - mean, 2.0F);
+        r->variance += powf((float) values[i] - r->mean, 2.0F);
     }
-    variance /= (float) size;
-
-    /* return array_info variable */
-    struct array_info info = {.min = min, .max = max, .mean = mean, .variance = variance};
-    return info;
+    r->variance /= (float) size;
 }
 
 #define SIZE 128
@@ -50,14 +49,6 @@ int main(void) {
     }
 
     /* call array_stats and show the result */
-    info = array_stats(src, SIZE);
+    array_stats(&info, src, SIZE);
     printf("max=%d min=%d mean=%.3f variance=%.3f\n", info.max, info.min, info.mean, info.variance);
-
-    return 0;
 }
-
-    
-
-
-    
-     
--- lab03/ex06.c
+++ lab03/ex06.c
@@ -5,5 +5,19 @@ struct complex {
 };
 
 void complex_product(struct complex *r, const struct complex *c1, const struct complex *c2){
-    
+    r->real = (c1->real * c2->real) - (c1->imaginary * c2->imaginary);
+    r-> imaginary = (c1->real * c2->imaginary) + (c1->imaginary * c2->real);
+}
+int main(){
+    struct complex c1 = {2.0, 3.0};  // c1 = 2 + 3i
+    struct complex c2 = {4.0, 5.0};  // c2 = 4 + 5i
+    struct complex result;
+
+    // Compute the product
+    complex_product(&result, &c1, &c2);
+
+    // Print the result
+    printf("Product: %.2f + %.2fi\n", result.real, result.imaginary);
+
+    return 0;
 }
